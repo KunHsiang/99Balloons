007280b37e31d38bdd30872f28b5614760a94b2a
ChangKH <web@kunhsiang.com>
2014-12-25 22:57:05 +0800

    add project

--- 99RedBallons/ViewController.swift
+++ 99RedBallons/ViewController.swift
@@ -10,8 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var balloons:[Balloon] = []
+    var currentIndex = 0
+    
+    @IBOutlet weak var ballonsLabel: UILabel!
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.createBallons()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +28,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func nextBalloonBarButtonItemPressed(sender: UIBarButtonItem) {
+        
+        let balloon = balloons[currentIndex]
+        
+        ballonsLabel.text = "\(balloon.number) balloon"
+        backgroundImageView.image = balloon.image
+        
+        currentIndex += 1
+    }
+    
+    func createBallons(){
+        
+        for var balloonCount = 0; balloonCount<=99 ; balloonCount++ {
+            
+            var balloon = Balloon()
+            balloon.number = balloonCount
+            let randomNumber = Int(arc4random_uniform(UInt32(4)))
+            switch randomNumber {
+            case 1:
+                balloon.image = UIImage(named:"RedBalloon1.jpg")
+            case 2:
+                balloon.image = UIImage(named:"RedBalloon2.jpg")
+            case 3:
+                balloon.image = UIImage(named:"RedBalloon3.jpg")
+            default:
+                balloon.image = UIImage(named:"RedBalloon4.jpg")
+            }
+            self.balloons.append(balloon)
+            
+            
+        }
+            
+        
+    }
 }
-
